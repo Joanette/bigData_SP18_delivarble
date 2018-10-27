d17f6904890af0cf13207a65c3e85539bc7963b7
Joanette <joanette.rosario@upr.edu>
2018-10-26 23:00:49 -0400

    p3b.py updated with final answer

--- spark/p3b.py
+++ spark/p3b.py
@@ -1,1 +1,38 @@
 #Code here
+#Find all the schools in the city of Ponce or in Cabo Rojo or in Dorado. 
+from pyspark import SparkContext 
+from pyspark.sql import SQLContext
+from pyspark.sql.functions import *
+from pyspark.sql.types import *
+
+sqlContext = SQLContext(sc)
+schemaESC = StructType([
+				StructField("regionEducativa", StringType(), True),
+				StructField("distritoEscolar", StringType(), True),
+                StructField("ciudad", StringType(), True),
+				StructField("schoolid", IntegerType(), True),
+				StructField("schoolName", StringType(), True),
+				StructField("nivel", StringType(), True),
+				StructField("CBid",IntegerType(), True),
+				]
+			)
+schemaStudents = StructType([
+				StructField("regionEducativa", StringType(), True),
+				StructField("distritoEscolar", StringType(), True),
+         		StructField("schoolid", IntegerType(), True),
+				StructField("schoolName", StringType(), True),
+				StructField("nivel", StringType(), True),
+				StructField("sexo",StringType(), True),
+				StructField("studentid",IntegerType(), True),
+				]
+			)
+escuelaPRdf = sqlContext.read\
+			 .options(header='true')\
+			 .schema(schemaESC)\
+			 .csv('escuelasPR.csv')
+estudiantesdf = sqlContext.read\
+			 .options(header='true')\
+			 .schema(schemaStudents)\
+			 .csv('studentsPR.csv')
+
+escuelaPRdf.filter((col("ciudad") == "Ponce") | (col("ciudad") == "Cabo Rojo") |(col("ciudad") == "Dorado") ).show()
